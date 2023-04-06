bb54fc10747b8a124d32e8417e9198e396157d6e
Rute Souza <rutesouza6801@gmail.com>
2023-04-05 23:52:42 -0300

    adding link to logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        <a href="https://ruteyacy.github.io/top-charts"><img src={logo} alt='logo' className="w-full h-15 object-contain"/></a>
+        <a href="https://ruteyacy.github.io/lyriks/"><img src={logo} alt='logo' className="w-full h-15 object-contain"/></a>
         <NavLinks />
       </div>
       <div className="absolute md:hidden block top-6 right-3">
